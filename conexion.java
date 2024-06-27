814387babaa3bf2970c14d66925c9852d4a01792
Ricardo Mora <rurimoca@gmail.com>
2024-06-26 23:03:05 -0600

    Clase de conexión y configuración

--- conexion.java
+++ conexion.java
@@ -1,46 +1,23 @@
 import java.io.*;
 import java.math.BigInteger;
 import java.net.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.*;
-import java.security.spec.X509EncodedKeySpec;
 
 import javax.crypto.*;
 import javax.crypto.spec.*;
-import javax.swing.JFileChooser;
-
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class conexion {
+public class Conexion {
 
     final static String SERVER_ADDRESS = "localhost";
     final static int SERVER_PORT = 12345;
     static Socket socket;
-    private static ObjectOutputStream objectOutputStream;
-    private static ObjectInputStream objectInputStream;
-    private static DataOutputStream dataOutputStream;
-    private static DataInputStream dataInputStream;
+    protected static ObjectOutputStream objectOutputStream;
+    protected static ObjectInputStream objectInputStream;
+    protected static DataOutputStream dataOutputStream;
+    protected static DataInputStream dataInputStream;
 
     private static Commands command = new Commands();
-
-    public static void main(String[] args) {
-        try {
-            connect();
-            setupKeys();
-            LoginScreen.initialize(args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void connect() throws Exception {
 
@@ -79,7 +56,7 @@ public class conexion {
 
         // Recibir clave pública de Bob
         PublicKey bobPublicKey = (PublicKey) objectInputStream.readObject();
-        if (!validatePublicKey(bobPublicKey)) {
+        if (!Utilidades.validatePublicKey(bobPublicKey)) {
             throw new IllegalArgumentException("Clave pública recibida es inválida");
         }
         System.out.println("Clave pública de Bob recibida y validada.");
@@ -98,7 +75,7 @@ public class conexion {
         // Calcular hash SHA-256 de la clave compartida
         MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
         byte[] sharedSecretHash = sha256.digest(sharedSecret);
-        System.out.println("Clave compartida hash (Alice): " + bytesToHex(sharedSecretHash));
+        System.out.println("Clave compartida hash (Alice): " + Utilidades.bytesToHex(sharedSecretHash));
     }
 
     protected static String login(String email, String password) {
@@ -123,42 +100,13 @@ public class conexion {
 
     protected static void displayUserInterface(String userType, Stage primaryStage) {
         if (userType.equals("administrador")) {
-            showAdminInterface(primaryStage);
+            AdminInterface.showAdminInterface(primaryStage);
         } else if (userType.equals("colaborador")) {
-            showColaboratorInterface(primaryStage);
+            ColaboratorInterface.showColaboratorInterface(primaryStage);
         }
     }
 
-    public static void showColaboratorInterface(Stage primaryStage) {
-        primaryStage.setTitle("Pantalla con 3 Botones");
-        Button btnFirmarAcuerdo = new Button("Firmar Acuerdo");
-        Button btnDesencriptarReceta = new Button("Desencriptar Receta");
-        Button btnSalir = new Button("Salir");
-
-        btnFirmarAcuerdo.setOnAction(e -> firmarAcuerdo());
-        btnDesencriptarReceta.setOnAction(e -> desencriptarReceta());
-        btnSalir.setOnAction(e -> {
-            cerrarConexion();
-            primaryStage.close();
-        });
-
-        VBox vbox = new VBox(btnFirmarAcuerdo, btnDesencriptarReceta, btnSalir);
-        Scene scene = new Scene(vbox, 300, 200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private static void firmarAcuerdo() {
-        try {
-            GenerateKeys.generate();
-            HashAndEncrypt.hashAndEncrypt();
-            enviarFirma();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    private static void cerrarConexion() {
+    protected static void cerrarConexion() {
         try {
             dataOutputStream.writeUTF(command.getFinishConnection());
             dataOutputStream.flush();
@@ -183,278 +131,5 @@ public class conexion {
             e.printStackTrace();
             System.err.println("No se pudo cerrar correctamente la conexión: " + e.getMessage());
         }
-    }
-
-    public static void showAdminInterface(Stage primaryStage) {
-        primaryStage.setTitle("File Sharing App");
-        Button subirButton = new Button("Subir");
-        Button compartirButton = new Button("Compartir");
-        Button validarButton = new Button("Validar");
-        Button agregarButton = new Button("Agregar usuario");
-        Button eliminarButton = new Button("Eliminar usuario");
-        Button salirButton = new Button("Salir");
-
-        subirButton.setOnAction(e -> subirArchivo());
-        compartirButton.setOnAction(e -> compartirArchivo());
-        validarButton.setOnAction(e -> validarArchivo());
-        agregarButton.setOnAction(e -> agregaUsuario());
-        eliminarButton.setOnAction(e -> eliminaUsuario());
-        salirButton.setOnAction(e -> {
-            cerrarConexion();
-            primaryStage.close();
-        });
-
-        VBox vbox = new VBox(10, subirButton, compartirButton, validarButton, agregarButton, eliminarButton,
-                salirButton);
-        Scene scene = new Scene(vbox, 300, 200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private static void validarArchivo() {
-        try {
-            // Aplicar hash SHA-256 al archivo 'm.txt'
-            byte[] fileHash;
-            fileHash = hashFile("received_m.txt");
-            PublicKey publicKey = getPublicKeyFromFile("receivedPublicKey.pem");
-
-            byte[] decryptedHash = decryptWithPublicKey("received_encrypted_hash.bin",
-                    publicKey);
-
-            // Aplicar hash SHA-256 al resultado del descifrado
-            byte[] decryptedHashFileHash = hashBytes(decryptedHash);
-
-            // Comparar los hashes
-            boolean isMatch = MessageDigest.isEqual(fileHash, decryptedHashFileHash);
-            System.out.println("Hash comparison result: " + (isMatch ? "MATCH" : "DO NOT MATCH"));
-            System.out.println("Validando archivo...");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void compartirArchivo() {
-        // Lógica para compartir archivo
-        System.out.println("Compartiendo archivo...");
-        // Aquí se debería implementar la lógica para enviar el archivo al cliente
-        // mediante sockets
-        // Ejemplo básico:
-        // enviarArchivoAlCliente();
-    }
-
-    private static void subirArchivo() {
-        // Crear un selector de archivos usando JFileChooser
-        JFileChooser fileChooser = new JFileChooser();
-        int returnValue = fileChooser.showOpenDialog(null);
-
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            try {
-                File selectedFile = fileChooser.getSelectedFile();
-
-                OutputStream outputStream;
-                outputStream = socket.getOutputStream();
-
-                FileInputStream fileInputStream = new FileInputStream(selectedFile);
-
-                long fileSize = selectedFile.length();
-                String fileName = selectedFile.getName();
-
-                // Enviar el nombre del archivo y su tamaño
-                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-                dataOutputStream.writeUTF(fileName);
-                dataOutputStream.writeLong(fileSize);
-                dataOutputStream.flush();
-
-                System.out.println("Enviando archivo: " + fileName + " de tamaño: " + fileSize + " bytes");
-
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
-                    outputStream.flush();
-                }
-
-                fileInputStream.close();
-
-                System.out.println("Archivo " + fileName + " enviado al servidor.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    private static void enviarFirma() throws IOException {
-        String[] filePaths = { "m.txt", "encrypted_hash.bin", "publicKey.pem" };
-        for (String filePath : filePaths) {
-            enviarArchivo(filePath);
-        }
-    }
-
-    private static void desencriptarReceta() {
-        // Implementación de desencriptación de receta
-    }
-
-    private static void agregaUsuario() {
-        // Crear una ventana para ingresar datos del usuario
-        Stage stage = new Stage();
-        GridPane grid = new GridPane();
-        TextField emailField = new TextField();
-        PasswordField passwordField = new PasswordField();
-        ComboBox<String> typeComboBox = new ComboBox<>();
-        typeComboBox.getItems().addAll("administrador", "colaborador");
-        Button addButton = new Button("Agregar");
-
-        grid.add(new Label("Email:"), 0, 0);
-        grid.add(emailField, 1, 0);
-        grid.add(new Label("Contraseña:"), 0, 1);
-        grid.add(passwordField, 1, 1);
-        grid.add(new Label("Tipo de Usuario:"), 0, 2);
-        grid.add(typeComboBox, 1, 2);
-        grid.add(addButton, 1, 3);
-
-        addButton.setOnAction(e -> {
-            try {
-                // Envía los datos del nuevo usuario al servidor
-                dataOutputStream.writeUTF(command.getAddUser());
-                dataOutputStream.flush();
-
-                dataOutputStream.writeUTF(emailField.getText());
-                dataOutputStream.flush();
-
-                dataOutputStream.writeUTF(passwordField.getText());
-                dataOutputStream.flush();
-
-                dataOutputStream.writeUTF(typeComboBox.getValue());
-                dataOutputStream.flush();
-
-                // Espera una respuesta del servidor
-                String serverResponse = dataInputStream.readUTF();
-                System.out.println("Respuesta del servidor: " + serverResponse);
-
-                stage.close(); // Cierra la ventana tras la respuesta
-            } catch (IOException ex) {
-                System.err.println("Error en el envío de parámetros al servidor: " + ex.getMessage());
-            }
-        });
-
-        Scene scene = new Scene(grid);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private static void eliminaUsuario() {
-        // Crear una ventana para eliminar un usuario
-        Stage stage = new Stage();
-        GridPane grid = new GridPane();
-        TextField emailField = new TextField();
-        Button deleteButton = new Button("Eliminar");
-
-        grid.add(new Label("Email del usuario a eliminar:"), 0, 0);
-        grid.add(emailField, 1, 0);
-        grid.add(deleteButton, 1, 1);
-
-        deleteButton.setOnAction(e -> {
-            try {
-                dataOutputStream.writeUTF(command.getDeleteUser());
-                dataOutputStream.flush();
-
-                dataOutputStream.writeUTF(emailField.getText());
-                dataOutputStream.flush();
-
-                // Espera una respuesta del servidor
-                String serverResponse = dataInputStream.readUTF();
-                System.out.println("Respuesta del servidor: " + serverResponse);
-
-                stage.close(); // Cierra la ventana tras la respuesta
-            } catch (IOException ex) {
-                System.err.println("Error en el envío de parámetros al servidor: " + ex.getMessage());
-            }
-        }); // Cierra la ventana tras la respuesta
-
-        Scene scene = new Scene(grid);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private static byte[] hashBytes(byte[] data) throws Exception {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        return digest.digest(data);
-    }
-
-    private static byte[] hashFile(String filePath) throws Exception {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
-        return digest.digest(fileBytes);
-    }
-
-    private static PublicKey getPublicKeyFromFile(String filePath) throws Exception {
-        byte[] publicKeyBytes = Files.readAllBytes(Paths.get(filePath));
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        return keyFactory.generatePublic(keySpec);
-    }
-
-    private static byte[] decryptWithPublicKey(String filePath, PublicKey publicKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, publicKey);
-
-        byte[] encryptedData = Files.readAllBytes(Paths.get(filePath));
-        return cipher.doFinal(encryptedData);
-    }
-
-    private static String bytesToHex(byte[] bytes) {
-        StringBuilder hexString = new StringBuilder();
-        for (byte b : bytes) {
-            String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1)
-                hexString.append('0');
-            hexString.append(hex);
-        }
-        return hexString.toString();
-    }
-
-    private static boolean validatePublicKey(PublicKey key) {
-        // Implementación de ejemplo: validar la especificación de la clave y cualquier
-        // otra propiedad necesaria
-        return key.getAlgorithm().equals("DH") && key.getEncoded().length > 0;
-    }
-
-    public static void enviarArchivo(String filePath) throws IOException {
-        dataOutputStream.writeUTF(command.getUploadFile());
-        dataOutputStream.flush();
-
-        File file = new File(filePath);
-        long fileSize = file.length();
-        String fileName = file.getName();
-
-        System.out.println("Enviando archivo: " + filePath + "de tamaño: " + fileSize + " bytes");
-
-        // Enviar el nombre del archivo y su tamaño
-        dataOutputStream.writeUTF(fileName);
-        dataOutputStream.writeLong(fileSize);
-        dataOutputStream.flush();
-
-        FileInputStream fileInputStream = new FileInputStream(filePath);
-
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-        long totalBytesRead = 0;
-
-        while (totalBytesRead < fileSize && (bytesRead = fileInputStream.read(buffer)) != -1) {
-            try {
-                dataOutputStream.write(buffer, 0, bytesRead);
-                dataOutputStream.flush();
-                totalBytesRead += bytesRead;
-                
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        fileInputStream.close();
-
-        System.out.println("Archivo enviado al servidor");
     }
 }
