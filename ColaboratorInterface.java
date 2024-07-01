6d9425a32028332a0b7755820643c55896f18796
Ricardo Mora <rurimoca@gmail.com>
2024-07-01 06:45:58 -0600

    Se agregan códigos corregidos

---
+++ ColaboratorInterface.java
@@ -0,0 +1,99 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.swing.JFileChooser;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ColaboratorInterface {
+
+    public static void showColaboratorInterface(Stage primaryStage) {
+        primaryStage.setTitle("Colaborador");
+        Button btnFirmarAcuerdo = new Button("Firmar Acuerdo");
+        Button btnRecibirReceta = new Button("Recibir receta");
+        Button btnDesencriptarReceta = new Button("Desencriptar Receta");
+        Button btnSalir = new Button("Salir");
+
+        btnFirmarAcuerdo.setOnAction(e -> firmarAcuerdo());
+        btnRecibirReceta.setOnAction(e -> desencriptarReceta());
+        btnDesencriptarReceta.setOnAction(e -> recibirarchivo());
+        btnSalir.setOnAction(e -> salir(primaryStage));
+
+        VBox vbox = new VBox(btnFirmarAcuerdo, btnDesencriptarReceta, btnSalir);
+        Scene scene = new Scene(vbox, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private static void firmarAcuerdo() {
+        try {
+            GenerateKeys.generate();
+            HashAndEncrypt.hashAndEncrypt();
+            enviarFirma();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void enviarFirma() throws IOException {
+        String[] filePaths = { "m.txt", "encrypted_hash.bin", "keys/publicKey.pem" };
+        for (String filePath : filePaths) {
+            Utilidades.enviarArchivo(filePath);
+        }
+    }
+
+    private static void desencriptarReceta() {
+        JFileChooser fileChooser = new JFileChooser();
+        int returnValue = fileChooser.showOpenDialog(null);
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+
+            // Llamar a la clase AESGCMDecryptor para descifrar el archivo
+            try {
+                File keyFile = new File("hasht.txt"); // Ruta al archivo de clave
+                File outputFile = new File("receta_descifrada.txt"); // Ruta al archivo de salida descifrado
+
+                AESGCMDecryptor.decryptFile(selectedFile, keyFile, outputFile);
+                System.out.println("Archivo descifrado correctamente.");
+            } catch (Exception e) {
+                System.err.println("Error al descifrar el archivo: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void recibirarchivo() {
+        try {
+            InputStream inputStream = Conexion.socket.getInputStream();
+            String filePath = "received_file"; // Nombre del archivo recibido
+            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                bufferedOutputStream.write(buffer, 0, bytesRead);
+            }
+
+            bufferedOutputStream.close();
+            inputStream.close();
+
+            System.out.println("Archivo recibido y guardado como: " + filePath);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void salir(Stage stage) {
+        Conexion.cerrarConexion();
+        stage.close();
+    }
+}
---
+++ GenerateKeys.java
@@ -0,0 +1,35 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GenerateKeys {
+    public static void generate() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        KeyPair pair = keyGen.generateKeyPair();
+        PrivateKey privateKey = pair.getPrivate();
+        PublicKey publicKey = pair.getPublic();
+
+        Files.write(Paths.get("keys/privateKey.pem"), privateKey.getEncoded());
+        Files.write(Paths.get("keys/publicKey.pem"), publicKey.getEncoded());
+
+         // Print the content of the private key file
+         byte[] privateKeyBytes = Files.readAllBytes(Paths.get("keys/privateKey.pem"));
+         System.out.println("Private Key:\n" + bytesToHex(privateKeyBytes));
+ 
+         // Print the content of the public key file
+         byte[] publicKeyBytes = Files.readAllBytes(Paths.get("keys/publicKey.pem"));
+         System.out.println("Public Key:\n" + bytesToHex(publicKeyBytes));
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
