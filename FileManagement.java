d16b77bd7f184452474a4a1ccdac0bd42f46d67e
Ricardo Mora <rurimoca@gmail.com>
2024-07-01 02:24:31 -0600

    Se agrega clase para manejo de archivos

---
+++ FileManagement.java
@@ -0,0 +1,77 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileManagement {
+
+    protected static void recibirArchivo() {
+        try {
+            String fileName = Conexion.dataInputStream.readUTF();
+            long fileSize = Conexion.dataInputStream.readLong();
+            String saveFilePath = "recibido_" + fileName;
+
+            System.out.println("Recibiendo archivo: " + fileName + " de tamaño: " + fileSize + " bytes");
+
+            try (FileOutputStream fileOutputStream = new FileOutputStream(saveFilePath);
+                    BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)) {
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                long totalBytesRead = 0;
+
+                while (totalBytesRead < fileSize && (bytesRead = Conexion.dataInputStream.read(buffer)) != -1) {
+                    bufferedOutputStream.write(buffer, 0, bytesRead);
+                    totalBytesRead += bytesRead;
+                }
+
+                bufferedOutputStream.flush(); // Asegurar que todataOutputStream los datos han sido escritos
+
+                if (totalBytesRead == fileSize) {
+                    System.out.println("Archivo recibido correctamente y guardado como " + saveFilePath);
+
+                } else {
+                    System.out.println("Error: El tamaño del archivo recibido (" + totalBytesRead
+                            + " bytes) no coincide con el tamaño esperado (" + fileSize + " bytes).");
+                }
+
+                fileOutputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected static void recibirArchivo(String saveFilePath) {
+        try {
+            String fileName = Conexion.dataInputStream.readUTF();
+            long fileSize = Conexion.dataInputStream.readLong();
+
+            System.out.println("Recibiendo archivo: " + fileName + " de tamaño: " + fileSize + " bytes");
+
+            try (FileOutputStream fileOutputStream = new FileOutputStream(saveFilePath);
+                    BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)) {
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                long totalBytesRead = 0;
+
+                while (totalBytesRead < fileSize && (bytesRead = Conexion.dataInputStream.read(buffer)) != -1) {
+                    bufferedOutputStream.write(buffer, 0, bytesRead);
+                    bufferedOutputStream.flush();
+                    totalBytesRead += bytesRead;
+                }
+                fileOutputStream.close();
+
+                if (totalBytesRead == fileSize) {
+                    System.out.println("Archivo recibido correctamente y guardado como " + saveFilePath);
+                } else {
+                    System.out.println("Error: El tamaño del archivo recibido (" + totalBytesRead
+                            + " bytes) no coincide con el tamaño esperado (" + fileSize + " bytes).");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
