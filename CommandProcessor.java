2b9e64d2efec66afe8a7bcbe377eb402f196eaf9
Ricardo Mora <rurimoca@gmail.com>
2024-07-01 02:24:18 -0600

    Se agrega clase para procesar comandos

---
+++ CommandProcessor.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class CommandProcessor {
+
+    public static void processCommand(String command) throws IOException {
+        switch (command) {
+            case "enviaArchivo":
+                FileManagement.recibirArchivo();
+                break;
+            case "terminaConexion":
+                Conexion.cerrarConexion();
+                break;
+            default:
+                Conexion.dataOutputStream.writeUTF("Comando desconocido");
+                break;
+        }
+    }
+}
