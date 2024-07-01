9644e2fb03e08c8b7e640c9cb3066033523b61fa
Ricardo Mora <rurimoca@gmail.com>
2024-07-01 02:20:56 -0600

    Main sin try/catch

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            Conexion.connect();
-            Conexion.setupKeys();
-            LoginScreen.initialize(args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Conexion.connect();
+        Conexion.setupKeys();
+        LoginScreen.initialize(args);
     }
 }
