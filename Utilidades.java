4f4c5c42200c78c7a63b99cd9a9b62f28d50a262
Ricardo Mora <rurimoca@gmail.com>
2024-07-01 02:22:10 -0600

    Utilidades redicidas para el colaborador

--- Utilidades.java
+++ Utilidades.java
@@ -10,8 +10,6 @@ import java.security.MessageDigest;
 import java.security.PublicKey;
 import java.security.spec.X509EncodedKeySpec;
 
-import javax.crypto.Cipher;
-
 public class Utilidades {
 
     private static Commands command = new Commands();
@@ -34,12 +32,8 @@ public class Utilidades {
         return keyFactory.generatePublic(keySpec);
     }
 
-    public static byte[] decryptWithPublicKey(String filePath, PublicKey publicKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, publicKey);
-
-        byte[] encryptedData = Files.readAllBytes(Paths.get(filePath));
-        return cipher.doFinal(encryptedData);
+    public static boolean validatePublicKey(PublicKey key) {
+        return key.getAlgorithm().equals("DH") && key.getEncoded().length > 0;
     }
 
     public static String bytesToHex(byte[] bytes) {
@@ -51,12 +45,6 @@ public class Utilidades {
             hexString.append(hex);
         }
         return hexString.toString();
-    }
-
-    public static boolean validatePublicKey(PublicKey key) {
-        // Implementación de ejemplo: validar la especificación de la clave y cualquier
-        // otra propiedad necesaria
-        return key.getAlgorithm().equals("DH") && key.getEncoded().length > 0;
     }
 
     public static void enviarArchivo(String filePath) throws IOException {
