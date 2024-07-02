146ca864fe4a1a3a83bbfa7ae7a59de90978612c
Ricardo Mora <rurimoca@gmail.com>
2024-07-02 07:33:39 -0600

    Corrección en descifrado de archivo

--- AESGCMDecryptor.java
+++ AESGCMDecryptor.java
@@ -7,26 +7,28 @@ import java.nio.file.Files;
 public class AESGCMDecryptor {
 
     public static void decryptFile(File inputFile, File keyFile, File outputFile) throws Exception {
-        // Leer la clave desde el archivo
         byte[] keyBytes = Files.readAllBytes(keyFile.toPath());
         SecretKeySpec secretKey = new SecretKeySpec(keyBytes, "AES");
 
-        // Configurar el cifrador AES GCM
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        byte[] iv = new byte[12]; // IV debe tener exactamente 12 bytes para AES GCM
-        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128, iv);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, gcmParameterSpec);
+        byte[] iv = new byte[12];
 
-        // Leer el archivo cifrado y descifrarlo
         try (FileInputStream fis = new FileInputStream(inputFile);
-                FileOutputStream fos = new FileOutputStream(outputFile);
-                CipherInputStream cis = new CipherInputStream(fis, cipher)) {
+             FileOutputStream fos = new FileOutputStream(outputFile)) {
 
+            fis.read(iv); // Read the IV from the beginning of the file
+
+            Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+            GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128, iv);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, gcmParameterSpec);
+
+            CipherInputStream cis = new CipherInputStream(fis, cipher);
             byte[] buffer = new byte[8192];
             int bytesRead;
             while ((bytesRead = cis.read(buffer)) != -1) {
                 fos.write(buffer, 0, bytesRead);
             }
+            fos.flush();
+            cis.close();
         }
     }
 }
